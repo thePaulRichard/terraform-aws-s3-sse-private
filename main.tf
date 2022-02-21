5697cbc38689128b1a4399913dc6529a8629ea93
Paul Richard <paulopaim@outlook.com>
2022-02-21 18:00:23 -0300

    Add bucket_key_enabled=true for kms config

--- main.tf
+++ main.tf
@@ -67,6 +67,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "kms" {
   bucket = aws_s3_bucket.private.id
 
   rule {
+    bucket_key_enabled = true
+
     apply_server_side_encryption_by_default {
       kms_master_key_id = element(aws_kms_key.s3.*.arn, count.index)
       sse_algorithm     = "aws:kms"
